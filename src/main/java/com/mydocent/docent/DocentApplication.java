2a89ac62ec83264f6f52c0168a3fe9dc2766db61
LeeHyunKyeong <115594388+LeeHyunKyeong@users.noreply.github.com>
2025-07-26 16:07:09 +0900

    initial commit

---
+++ src/main/java/com/mydocent/docent/DocentApplication.java
@@ -0,0 +1,13 @@
+package com.mydocent.docent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocentApplication.class, args);
+	}
+
+}
